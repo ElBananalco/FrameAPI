6f66f9e8a85fd50bbfaaa8e284b8a63f6af326c3
ElBananalco <lm.ivancarvajal@gmail.com>
2020-03-19 13:54:38 -0600

    first

---
+++ src/test/java/Frame/Runner.java
@@ -0,0 +1,144 @@
+package Frame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+import junit.framework.TestCase;
+
+public class Runner extends TestCase{
+	
+	   public static void main(String args[]) throws IOException {
+			//creamos run para ejecutar los TC
+			TestMethods run = new TestMethods();
+			
+			//creamos este array para obtener todos los TC
+			ArrayList<TestData> ListaObjetosTestC = new ArrayList();
+			
+			//Aqui se agregaran los TC que seleccionemos
+			ArrayList<TestData> ListaTemp = new ArrayList();
+			
+			//Asignamos la URI de la pagina
+			String URL = "https://restool-sample-app.herokuapp.com/api/";
+			String  PATH ="employee";
+			
+			//Inicializamos la clase 
+			run.classSetup(URL, PATH);
+			
+			//creamos TC para leer el archivo Xlsx
+			leer TC = new leer();	
+			
+			//Cargamos la ruta que querramos
+	    	javax.swing.JFileChooser jF1= new javax.swing.JFileChooser();
+	    	
+	    	//aqui guardamos la ruta
+	    	String ruta = "";
+	    	try{
+	    	if(jF1.showSaveDialog(null)==jF1.APPROVE_OPTION){
+	    		//se guarda la ruta
+	    	ruta = jF1.getSelectedFile().getAbsolutePath();    	
+	    	}
+	    	}catch (Exception ex){
+	    	ex.printStackTrace();
+	    	}
+		
+	    	//se guardan los datos el excel a un arraylist
+			ListaObjetosTestC=TC.obtenerObjetos(ruta);
+			
+			//este arreglo guarda los nombres de los TC
+			String[] prueba = new String[ListaObjetosTestC.size()];
+			
+			//creamos un arreglo de radiobuttons
+			JRadioButton[] radio = new JRadioButton[ListaObjetosTestC.size()];
+			
+			//este for guardara los nombres TC en el arreglo prueba
+			for(int x=1;x<ListaObjetosTestC.size();x++) {
+				
+				if(ListaObjetosTestC.get(x) == null) {
+				
+				}else
+					//se guardan los nombres TC
+					prueba[x] =  run.testCases(ListaObjetosTestC.get(x));
+		
+			
+			}
+		
+			
+			String name="";
+			//creamos la pantalla
+			 JPanel panel = new JPanel();
+			 
+			//le damos formato
+			 panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); 
+				JLabel label = new JLabel();
+				  label.setText("Cuales TC deseas ejecutar");
+				  panel.add(label);
+				// Se le asigna una posición dentro del Jpanel 
+				  panel.setBounds(10, 10, 375, 150);
+				 
+				 //en este for recorremos el arrego de prueba para crear los radio button con los
+				  // nonbres de los TC
+			for(int u =0; u<prueba.length;u++) {
+				u++;
+				name = prueba[u];
+				 JRadioButton button = new JRadioButton(name);
+				radio[u]=button;
+		       
+				//agregamos el radiobutton al panel
+				panel.add(radio[u]);   
+				
+				
+			}
+			
+			//mostramos el panel
+			JOptionPane.showMessageDialog(null, panel);
+			
+			//este arreglo sera para guardar los que seleccionemos
+			String[] cad = new String[radio.length];
+			
+			//este for recorre el arreglo de los radio buttons para checar quien fue seleccionado
+			for(int t =0; t<radio.length;t++) {
+				t++;
+				//preguntamos si fue seleccionado
+	        if (radio[t].isSelected()) {
+	        	//guardamos el nombre del TC en el arreglo cad
+				   cad[t]=radio[t].getLabel();
+				  }
+			}
+		
+	
+			
+	
+	//juntamos los TC seleccionados en el arraylist
+	for(int sheet=0;sheet<cad.length;sheet++) {
+		if(cad[sheet]==null) {
+			
+		}else {
+			//el metdodo obtenerTC carga al array la hoja que le mandamos
+	   ListaTemp=TC.obtenerTC(ruta, cad[sheet]);
+		}
+	
+	}	
+	
+	//ejecutamos
+	for(int q=0;q<ListaTemp.size();q++) {
+		
+		run.executionFile(ListaTemp.get(q));
+}
+
+		
+		
+			
+			
+			
+			
+	    }
+	
+	
+}
---
+++ src/test/java/Frame/TestMethods.java
@@ -0,0 +1,67 @@
+package Frame;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class TestMethods {
+
+	public void classSetup(String URL, String path) {
+		try {
+			RestAssured.baseURI = URL;
+			RestAssured.basePath = path;
+		} catch (Exception e) {
+			System.out.print(e.getMessage());
+		}
+	}
+
+	
+	@Test
+	public void executionFile(TestData Obj) {
+		
+		int code,expected;
+		switch(Obj.getAccion().toLowerCase()) {
+			
+		case "post":
+			Response resp = RestAssured.given().header("content-type", "application/json").body(Obj.getBody()).post();
+			validationCode(Obj.getExpected(),resp.statusCode(),Obj.getNameTC());
+			
+			break;
+		case "get":
+			Response resp2 = RestAssured.given().header("content-type", "application/json").get();
+			validationCode(Obj.getExpected(),resp2.statusCode(),Obj.getNameTC());
+			
+			break;
+		
+		}
+
+	}
+	
+	public void validationCode(int expected, int code, String name) {
+		if(expected==code) {
+			
+			System.out.println("Test Case Pass "+name);
+			System.out.println("Status code expected is "+expected);
+			System.out.println("Status code is "+code);
+		}else {
+			System.out.println("Test Case Fail "+name);
+			System.out.println("Status code expected is "+expected);
+			System.out.println("Status code is "+code);
+		}
+		
+	}
+ 
+	public String testCases(TestData Obj){
+		String sheet="";
+		if( Obj.getNameTC()=="") {
+		
+		}else {
+			sheet = Obj.getNameTC();
+		}
+		
+		return sheet;
+	}
+	
+}
